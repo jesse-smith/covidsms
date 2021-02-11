b19f98863c7bfeb838656825c2cb740b378eb7d8
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-02-11 17:38:36 -0600

    Change `filter` default to TRUE

--- R/filter_by_new.R
+++ R/filter_by_new.R
@@ -1,4 +1,4 @@
 
-filter_by_new <- function(.data, filter = FALSE, date = attr(.data, "date")) {
+filter_by_new <- function(.data, filter = TRUE, date = attr(.data, "date")) {
   if (filter) dplyr::filter(.data, .data[["date_added"]] == date) else .data
 }
