c9937de452522a82a1833720dae4e2ddd5d7aa82
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-02-25 16:10:38 -0600

    Add `quiet` argument to `std_phone()` (default `TRUE`)

--- R/std_phone.R
+++ R/std_phone.R
@@ -10,10 +10,13 @@
 #' @param dialr Should the `dialr` package be used to check for valid number
 #'   ranges? The default is `FALSE`.
 #'
+#' @param quiet Should the progress bar from dialr be shown? Ignored if
+#'   `dialr = FALSE`.
+#'
 #' @return A `character` vector of standardized phone numbers
 #'
 #' @export
-std_phone <- function(x, dialr = FALSE) {
+std_phone <- function(x, dialr = FALSE, quiet = TRUE) {
 
   extraction_pattern <- "^([+]?0?1)?[0-9]{10}"
   removal_pattern <- "^([+]?0?1)(?=[0-9]{10})"
@@ -25,15 +28,32 @@ std_phone <- function(x, dialr = FALSE) {
     stringr::str_remove(pattern = removal_pattern)
 
   if (dialr) {
-
     coviData::assert_all(
       rlang::is_installed("dialr"),
-      message = "The `dialr` package must be installed to this feature"
+      message = "The `dialr` package must be installed to use this feature"
     )
+  }
 
-    ph %>%
-      dialr::phone(region = "US") %>%
-      format(format = "NATIONAL", clean = TRUE, strict = TRUE)
+  if (dialr && quiet) {
+    # Wrap dialr in expression to conditionally prevent status updates
+    ph_expr <- rlang::expr({
+      ph_out <- format(
+        dialr::phone(ph, region = "US"),
+        format = "NATIONAL",
+        clean = TRUE,
+        strict = TRUE
+      )
+    })
+    # Evaluate expression; saves output to `ph_out`
+    invisible(utils::capture.output(eval(ph_expr)))
+    ph_out
+  } else if (dialr) {
+    format(
+      dialr::phone(ph, region = "US"),
+      format = "NATIONAL",
+      clean = TRUE,
+      strict = TRUE
+    )
   } else {
     ph
   }
