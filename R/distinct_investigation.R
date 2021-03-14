c1c775ad495c0c076a609d1c61c8b271cd8adf17
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-14 11:17:28 -0500

    Add `nbs` column with `inv_local_id` to positive data

--- R/distinct_investigation.R
+++ R/distinct_investigation.R
@@ -21,20 +21,14 @@ distinct_investigation <- function(.data) {
       .lab_order_tmp_ = !.data[[".lab_tmp_"]] %in% labs
     ) %>%
     dplyr::arrange(
-      .data[[".inv_id_tmp_"]],
+      .data[["nbs"]],
       .data[[".rpt_dt_tmp_"]],
       .data[[".test_dt_tmp_"]],
       .data[[".lab_order_tmp_"]]
     ) %>%
-    coviData::coalesce_dupes(.data[[".inv_id_tmp_"]]) %>%
+    coviData::coalesce_dupes(.data[["nbs"]]) %>%
     dplyr::arrange(.data[[".row_id_tmp_"]]) %>%
     dplyr::select(
-      -c(
-        ".row_id_tmp_",
-        ".rpt_dt_tmp_",
-        ".test_dt_tmp_",
-        ".lab_order_tmp_",
-        ".inv_id_tmp_"
-      )
+      -c(".row_id_tmp_", ".rpt_dt_tmp_", ".test_dt_tmp_", ".lab_order_tmp_")
     )
 }
